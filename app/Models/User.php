f26b119acff965b6308a835d56a3a980a96150b7
Abu Husein <huseinabu01@gmail.com>
2025-01-20 07:12:32 +0800

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 
     protected $table = 'users'; // Pastikan sesuai dengan nama tabel di database
 
-    protected $fillable = ['name', 'email', 'password', 'admin_name'];
+    protected $fillable = ['name', 'email', 'password', 'admin_id'];
 
     public function admin()
     {
