613d7ff4328036172c5c08b76c97ed8bf50a2a96
Abu Husein <huseinabu01@gmail.com>
2025-01-20 10:31:05 +0800

    fix view video user

--- resources/views/staff/videos/index.blade.php
+++ resources/views/staff/videos/index.blade.php
@@ -9,23 +9,37 @@
             </div>
 
             <div class="row py-5">
-
                 @foreach ($data as $video)
                     <div class="kotak-video col-lg-4 mb-3">
                         <div class="card box-video">
-                            <iframe width="100%" height="215" src="{{ $video->iframe_video }}"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            @if (!empty($video->iframe_video))
+                                <iframe width="100%" height="215" 
+                                    src="{{ $video->iframe_video }}" 
+                                    title="YouTube video player" 
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerpolicy="strict-origin-when-cross-origin" 
+                                    allowfullscreen>
+                                </iframe>
+                            @elseif (!empty($video->file_video))
+                                <video width="100%" height="215" controls>
+                                    <source src="{{ asset('storage/' . $video->file_video) }}" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                            @endif
+
                             <div class="card-body judul-video">
                                 <h6 class="card-title">{{ $video->judul }}</h6>
-                                <p class="card-text">{{ $video->deskripsi }}</p> 
-                                <p class="card-text"><small class="text-body-secondary">{{ $video->updated_at->format('d M Y H:i')  }}</small></p>
+                                <p class="card-text">{{ $video->deskripsi }}</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary">
+                                        {{ $video->updated_at->format('d M Y') }}
+                                    </small>
+                                </p>
                             </div>
                         </div>
                     </div>
                 @endforeach
-
             </div>
         </div>
     </section>
